41140ae6bb7442c9fddb92a2d296eb5c5c31e6d0
Moses Kayanda <55259290+mkayanda@users.noreply.github.com>
2020-08-09 20:56:46 -0700

    Update Data Visualization with ggplot2.R

--- Data Visualization with ggplot2.R
+++ Data Visualization with ggplot2.R
@@ -1,24 +1,5 @@
-Copyright 2017 Data Science Dojo
-#    
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# 
 
-
-#
-# This R source code file corresponds to the Data Science Dojo webinar 
-# titled "An Introduction to Data Visualization with R and ggplot2" 
-#
-setwd("C:/Users/Arham/Desktop/Data Visualization with ggplot2")
+setwd("../Data Visualization with ggplot2")
 install.packages("ggplot2")
 install.packages("dplyr")
 library(dplyr)
